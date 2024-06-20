31b6b4be52aee5aa81b0d18ff25af46c055d173a
dranikk <dimondrachuk08@gmail.com>
2024-06-20 16:39:31 +0300

    Project

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();        
+        }
+        public MyCollection<object> GetCollection()
+        {
+           
+            return new MyCollection<object>(); 
+        }
+
+        int index = 0;
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            GetCollection().Add(textBox1.Text);
+            index++;
+            GetCollection().Get(index);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = Convert.ToString(GetCollection());
+        }
+        int index1 = 0;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            index1 = Convert.ToInt32(textBox2.Text);
+            GetCollection().Remove(index, index1);
+        }
+    }
+}
---
+++ MyCollection.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp8
+{
+    public class MyCollection<T>
+    {
+        private List<T> items;
+        public MyCollection()
+        {
+            items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+        //&& index<items.Count
+        public T Get(int index)
+        {
+            if(index >= 0 )
+            {
+                return items[index];
+            }
+            else
+            {
+               throw new IndexOutOfRangeException("Індекс поза межами колекції!");            
+            }
+        }
+        public void Remove(int index, int index1)
+        {
+            if (index1 > 0 && index < items.Count)
+            {
+                items.RemoveAt(index1);
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Індекс поза межами колекції!");
+            }
+        }
+        
+        
+        public override string ToString()
+        {
+            return items.ToString();
+        }
+    }
+}
